364f61836b21562aca991b046cecbe3ae4a4d0f5
Lucas Shoiti Takahashi <lucas.shoiti@hotmail.com>
2020-08-12 20:02:53 -0300

    metodos da assinatura implementados

--- MulticastPeer.java
+++ MulticastPeer.java
@@ -1,6 +1,13 @@
 import java.net.*;
+import java.security.SignatureException;
+import java.security.PublicKey;
 import java.util.List;
+import java.security.spec.InvalidKeySpecException;
+
 import java.util.Scanner;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
 
 import javax.sound.sampled.SourceDataLine;
 
@@ -13,22 +20,38 @@ public class MulticastPeer{
 
 		try {
 			InetAddress group = InetAddress.getByName("228.5.6.7");
-			s = new MulticastSocket(6789);
-			s.joinGroup(group);
-			String message = "er";
+			Assinatura assinatura = new Assinatura();
+			assinatura.createKeys();
+			PublicKey chave = assinatura.getPubKey();
 
-			while(true){
-				DatagramPacket messageOut = new DatagramPacket(
-					message.getBytes(), 2, group, 6789);
-			  	s.send(messageOut);
-				byte[] buffer = new byte[1000];
-				for(int i=0; i< 4;i++) {		// get messages from others in group
-					DatagramPacket messageIn = new DatagramPacket(buffer, buffer.length);
-					s.receive(messageIn);
-			   	    System.out.println("Received:" + new String(messageIn.getData()));	 
-			    }
-			    break;  
-			}
+			System.out.println(chave);	
+
+
+			// String mensagem = "handshake|" + "ident|" + Base64.getEncoder().encodeToString(chave);
+
+			// DatagramPacket messageOut = new DatagramPacket(
+			// 		message.getBytes(), 2, group, 6789);
+			//   	s.send(messageOut);
+
+			
+
+			// while(true){
+			// 	System.Out.Println("Digite uma opção: ");
+			//  	System.Out.Println("0 - Ler mensagens");
+			//  	System.Out.Println("1 - Digitar uma notícia");
+			//  	System.Out.Println("2 - Denunciar uma fake news");
+			//  	System.Out.Println("3 - Sair");
+			// 	DatagramPacket messageOut = new DatagramPacket(
+			// 		message.getBytes(), 2, group, 6789);
+			//   	s.send(messageOut);
+			// 	byte[] buffer = new byte[1000];
+			// 	for(int i=0; i< 4;i++) {		// get messages from others in group
+			// 		DatagramPacket messageIn = new DatagramPacket(buffer, buffer.length);
+			// 		s.receive(messageIn);
+			//    	    System.out.println("Received:" + new String(messageIn.getData()));	 
+			//     }
+			//     break;  
+			// }
 			// int escolha = 10;
 			
 			// while(escolha != 3){
@@ -69,9 +92,11 @@ public class MulticastPeer{
 			// 			System.Out.Println("Escolha inválida!");
 			// 			break;
 			// 	}	
-			//}											
-		}catch (SocketException e){System.out.println("Socket: " + e.getMessage());
+			//}
+			 											
+		//}catch (SocketException e){System.out.println("Socket: " + e.getMessage());
 		}catch (IOException e){System.out.println("IO: " + e.getMessage());
+		}catch (NoSuchAlgorithmException e){System.out.println("IO: " + e.getMessage());
 		}finally {if(s != null) s.close();}
 	}		      	
 	
